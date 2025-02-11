3224f4b5484d649c7ef782a2c2416809a55f7438
Dileep_742 <167205876+VGNANADILEEPREDDY@users.noreply.github.com>
2025-02-11 17:21:25 +0530

    new

---
+++ homepage/js/homepage.js
@@ -0,0 +1,79 @@
+// start get brand code from localstorage 
+
+var i;
+var allBrandKey = [];
+for(i=0;i<localStorage.length;i++){
+    var allKeys = localStorage.key(i);
+    if(allKeys.match("_brand")){
+        allBrandKey.push(
+            allKeys.replace("_brand","")
+        );
+    }
+
+}
+
+// create option coding for brandcode 
+var brandCodeEl = document.querySelector("#brand-code-el");
+allBrandKey.forEach((code,index)=>{
+    brandCodeEl.innerHTML += `
+    <option value="${code}">${code}</option>
+
+    `;
+})
+
+// all global varible
+var loginForm = document.querySelector(".login-form");
+var allLoginInput = loginForm.querySelectorAll("input");
+var loginBtn = loginForm.querySelector("button");
+var brandCode;
+var allUserData = [];
+// start login coding
+brandCodeEl.addEventListener('change',()=>{
+    if(brandCodeEl.value != "choose space code"){
+        sessionStorage.setItem("brandCode",brandCodeEl.value);
+        allLoginInput[0].disabled = false;
+        allLoginInput[1].disabled = false;
+        loginBtn.disabled = false;
+        brandCode = sessionStorage.getItem("brandCode");
+        loginUserFun();
+    }else{
+        allLoginInput[0].disabled = true;
+        allLoginInput[1].disabled = true;
+        loginBtn.disabled = true;
+        swal("please select brand!", "Please select brand code!", "warning");
+    }
+});
+
+const loginUserFun = () =>{
+    if(localStorage.getItem(brandCode+"_registrationData") != null){
+        allUserData = JSON.parse(localStorage.getItem(brandCode+"_registrationData"));
+    }
+    console.log(allUserData);
+    loginForm.onsubmit = function(e){
+        e.preventDefault();
+        for(i=0;i<allUserData.length;i++){
+            if(allUserData[i].enrollment == allLoginInput[0].value){
+                if(allUserData[i].password == allLoginInput[1].value){
+                    if(allUserData[i].userType == "teacher"){
+                        sessionStorage.setItem("brandCode",brandCode);
+                        window.location = "../dashboard/dashboard.html";
+                    }else{
+                        sessionStorage.setItem("enrollment",allUserData[i].enrollment);
+                        sessionStorage.setItem("name",allUserData[i].name);
+                        sessionStorage.setItem("address",allUserData[i].address);
+                        sessionStorage.setItem("fatherName",allUserData[i].fatherName);
+                        sessionStorage.setItem("brandCode",brandCode);
+                        sessionStorage.setItem("imgUrl",allUserData[i].profilePic);
+                        window.location = "../welcome/welcome.html";
+                    }
+                }else{
+                    swal("Wrong Password", "Please contact your teacher !", "warning");
+                    return;
+                }
+                return;
+            }else{
+                swal("Wrong enrollment", "Please contact your teacher !", "warning");
+            }
+        }
+    }
+}
